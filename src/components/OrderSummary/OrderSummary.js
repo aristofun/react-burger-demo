89da202516c59fbdb1906f6f588893d4cfd0e139
aristofun <mbutlitsky@gmail.com>
2019-11-07 20:48:13 +0200

    section 8 finished

--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -1,27 +1,29 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, Component} from 'react';
 
 import classes from './OrderSummary.module.css';
 import Button from "../Visual/Button";
 
-const OrderSummary = (props) => {
-  const ings = Object.keys(props.ingredients)
-    .map(igKey => <li key={igKey}><span style={{ textTransform: 'capitalize' }}>{igKey}</span>:
-      {props.ingredients[igKey]}</li>);
+class OrderSummary extends Component {
+  render() {                   
+    const ings = Object.keys(this.props.ingredients)
+      .map(igKey => <li key={igKey}><span style={{ textTransform: 'capitalize' }}>{igKey}</span>:
+        {this.props.ingredients[igKey]}</li>);
 
-  return (
-    <Fragment>
-      <h3>Your order</h3>
-      <p>Nice burger with ingredients:</p>
-      <ul>
-        {ings}
-      </ul>
+    return (
+      <Fragment>
+        <h3>Your order</h3>
+        <p>Nice burger with ingredients:</p>
+        <ul>
+          {ings}
+        </ul>
 
-      <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
+        <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
 
-      <Button btnType='Danger' onClick={props.cancelled}>Cancel</Button>
-      <Button btnType='Success' onClick={props.continued}>Continue</Button>
-    </Fragment>
-  );
-};
+        <Button btnType='Danger' onClick={this.props.cancelled}>Cancel</Button>
+        <Button btnType='Success' onClick={this.props.continued}>Continue</Button>
+      </Fragment>
+    );
+  }
+}
 
 export default OrderSummary;
